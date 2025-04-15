07d017b75c8e663010a1842136cc2ef44e64445f
phanindra <phanidevlancer@gmail.com>
2025-04-15 08:29:12 +0530

    mongodb added

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://admin:phani2205@cruddb.qhdtlak.mongodb.net/?retryWrites=true&w=majority&appName=crudDB')
+    .then(() => console.log('Connected!'));
+
 
 app.listen(3000, () => {
     console.log("Hello world")
 })
 
-app.get('/', (req, res) => {
-    res.send("Hello world! with nodeMon")
+app.post('/api/products', (req, res) => {
+    console.log('Body is ', req.body)
+    res.send(req.body)
 })
+
+app.get('/', (req, res) =>
+    res.send("Hello world! with nodeMon")
+)
---
+++ models/product.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Product name is required'],
+    },
+    quantity: {
+        type: Number,
+        required: [true, 'Product quantity is required'],
+        default: 0,
+    },
+    price: {
+        type: Number,
+        required: [true, 'Product price is required'],
+        default: 0,
+    },
+    image: {
+        type: String,
+        required: false,
+    }
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
